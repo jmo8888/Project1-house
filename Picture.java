f11b9d8704e93319612dd348df4917a081238b99
Jennifer Moran <jmoran@cheapcaribbean.com>
2015-09-17 12:00:29 -0400

    Lab 1 Assignment
    Changes picture per lab 1 assignment

--- Picture.java
+++ Picture.java
@@ -6,15 +6,25 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael Kï¿½lling and David J. Barnes
  * @version 2011.07.31
  */
 public class Picture
 {
-    private Square wall;
+    private Square house;
+    private Square houseHider;
     private Square window;
+    private Square windowHider;
+    private Square pane1;
+    private Square pane2;
+    private Square pane3;
+    private Square pane4;
     private Triangle roof;
     private Circle sun;
+    private Square door;
+    private Square doorHider;
+    private Circle doorKnob;
+    private Person me;
 
     /**
      * Constructor for objects of class Picture
@@ -29,31 +39,101 @@ public class Picture
      */
     public void draw()
     {
-        wall = new Square();
-        wall.moveHorizontal(-140);
-        wall.moveVertical(20);
-        wall.changeSize(120);
-        wall.makeVisible();
+        house = new Square();
+        house.moveHorizontal(-220);
+        house.moveVertical(50);
+        house.changeSize(180);
+        house.makeVisible();
         
+        houseHider = new Square();
+        houseHider.changeColor("white");
+        houseHider.moveHorizontal(-65);
+        houseHider.moveVertical(50);
+        houseHider.changeSize(160);
+        houseHider.makeVisible();    
+   
         window = new Square();
         window.changeColor("black");
-        window.moveHorizontal(-120);
-        window.moveVertical(40);
-        window.changeSize(40);
-        window.makeVisible();
-
+        window.moveHorizontal(-210);
+        window.moveVertical(70);
+        window.changeSize(80);
+        window.makeVisible();    
+        
+        windowHider = new Square();
+        windowHider.changeColor("red");
+        windowHider.moveHorizontal(-145);
+        windowHider.moveVertical(70);
+        windowHider.changeSize(80);
+        windowHider.makeVisible();
+        
+        pane1 = new Square();
+        pane1.changeColor("white");
+        pane1.moveHorizontal(-205);
+        pane1.moveVertical(80);
+        pane1.changeSize(25);
+        pane1.makeVisible();
+        
+        pane2 = new Square();
+        pane2.changeColor("white");
+        pane2.moveHorizontal(-175);
+        pane2.moveVertical(80);
+        pane2.changeSize(25);
+        pane2.makeVisible();        
+        
+        pane3 = new Square();
+        pane3.changeColor("white");
+        pane3.moveHorizontal(-205);
+        pane3.moveVertical(115);
+        pane3.changeSize(25);
+        pane3.makeVisible();
+        
+        pane4 = new Square();
+        pane4.changeColor("white");
+        pane4.moveHorizontal(-175);
+        pane4.moveVertical(115);
+        pane4.changeSize(25);
+        pane4.makeVisible();         
+        
         roof = new Triangle();  
-        roof.changeSize(60, 180);
-        roof.moveHorizontal(20);
+        roof.changeColor("black");
+        roof.changeSize(90, 180);
+        roof.moveHorizontal(-42);
         roof.moveVertical(-60);
-        roof.makeVisible();
-
+        roof.makeVisible();      
+        
+        door = new Square();
+        door.changeColor("yellow");
+        door.moveHorizontal(-100);
+        door.moveVertical(120);
+        door.changeSize(60);
+        door.makeVisible();          
+        
+        doorHider = new Square();
+        doorHider.changeColor("white");
+        doorHider.moveHorizontal(-64);
+        doorHider.moveVertical(120);
+        doorHider.changeSize(60);
+        doorHider.makeVisible();   
+        
+        doorKnob = new Circle();
+        doorKnob.changeColor("green");
+        doorKnob.moveHorizontal(0);
+        doorKnob.moveVertical(170);
+        doorKnob.changeSize(10);
+        doorKnob.makeVisible();    
+        
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(100);
+        sun.moveHorizontal(120);
         sun.moveVertical(-40);
         sun.changeSize(80);
-        sun.makeVisible();
+        sun.makeVisible();        
+        
+        me = new Person();
+        me.moveHorizontal(120);
+        me.moveVertical(67);
+        me.makeVisible();       
+        
     }
 
     /**
@@ -61,9 +141,9 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        if (wall != null)   // only if it's painted already...
+        if (house != null)   // only if it's painted already...
         {
-            wall.changeColor("black");
+            house.changeColor("black");
             window.changeColor("white");
             roof.changeColor("black");
             sun.changeColor("black");
@@ -75,9 +155,9 @@ public class Picture
      */
     public void setColor()
     {
-        if (wall != null)   // only if it's painted already...
+        if (house != null)   // only if it's painted already...
         {
-            wall.changeColor("red");
+            house.changeColor("red");
             window.changeColor("black");
             roof.changeColor("green");
             sun.changeColor("yellow");
